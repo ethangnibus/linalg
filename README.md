3235cbc11de6a53ff99a2a0d0679bab289615297
ethangnibus <ethangnibus@berkeley.edu>
2023-12-05 23:30:56 -0800

    drew window, camera, and background color to webpage

---
+++ README.md
@@ -0,0 +1,2 @@
+To run, use:
+cargo run --target wasm32-unknown-unknown
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct MyCameraMarker;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        MyCameraMarker,
+    ));
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
 fn main() {
-    App::new().run();
+    App::new()
+        //set the global default
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.3, 0.6)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup, setup_camera))
+        .run();
 }
